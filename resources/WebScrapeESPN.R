d2edaefd3e6366da4e4a6829a92c0de6b55941ee
mr-wellick <lorenz3192@gmail.com>
2024-08-25 23:46:23 -0700

    update file names

--- resources/WebScrapeESPN.R
+++
@@ -1,9 +0,0 @@
-# collect all urls for each top competition (soccer)
-# these appear in the following form: https://www.espn.com/soccer/teams/_/league/usa.1
-
-# use r-vest to web-scrape all teams for each top competition using above urls
-# create a data frame with the following columns
-# - team name
-# - league name
-# - country code
-# - division
