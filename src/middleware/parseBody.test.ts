898cba023a17eea0aa1f3498833b863913a164e5
mr-wellick <lorenz3192@gmail.com>
2024-09-05 17:09:17 -0700

    NPM test script
    	- can run tests that are written in ts!

--- src/middleware/parseBody.test.ts
+++ src/middleware/parseBody.test.ts
@@ -1,7 +1,7 @@
 import test from "node:test";
 
 test(
-  "parse body for an incomping request, save it, and forward parse info to controller",
+  "parse body for an incomping request, save it, and forward info to controller",
   { todo: true },
   (t) => {
     console.log("I am a test");
