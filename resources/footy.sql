0ef4b9f7bb5a0a1b1a6f5a11ac14bf2f54c8c204
mr-wellick <lorenz3192@gmail.com>
2024-09-07 01:19:31 -0700

    fix typos and rename file

--- resources/footy.sql
+++ resources/footy.sql
@@ -1,39 +1,23 @@
--- 1. https://supabase.com/blog/choosing-a-postgres-primary-key
--- 2. https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
--- 3. https://stackoverflow.com/questions/42193716/datatype-for-country-in-mysql
--- 4. https://sql.toad.cz/?keyword=default
--- 5. https://stackoverflow.com/questions/51463706/can-somebody-give-a-practical-example-of-a-many-to-many-relationship
--- 6. Reference OWASP documentation
-
 -- create countries table
 create table if not exists countries(
-   /*
-        A primary key constraint indicates that a column, or group of columns, can be used as a unique identifier for rows in the table.
-        This requires that the values be both unique and not null.
-   */
-   /*
-        Adding a primary key will automatically create a unique B-tree index on the column or group of columns
-        listed in the primary key, and will force the column(s) to be marked NOT NULL.
-   */
-    -- Relational database theory dictates that every table must have a primary key. This rule is not enforced by PostgreSQL, but it is usually best to follow it.
 	country_id uuid default gen_random_uuid() primary key,
 	country_code varchar(3) unique not null,
 	country_name varchar(100) unique not null
-)
+);
 
 -- create leagues table
 create table if not exists leagues(
 	league_id uuid default gen_random_uuid() primary key,
 	league_name varchar(100) not null,
-    country_id uuid references countries(country_id) not null,
-)
+    country_id uuid references countries(country_id) not null
+);
 
 -- create seasons years
 create table if not exists seasons(
 	season_id uuid default gen_random_uuid() primary key,
 	start_year int not null,
 	end_year int check (end_year > start_year) not null
-)
+);
 
 -- create players table
 create table if not exists players(
@@ -49,8 +33,8 @@ create table if not exists players(
 create table if not exists teams(
 	team_id uuid default gen_random_uuid() primary key,
 	name varchar(100) not null,
-    league_id uuid references leagues(league_id) not null,
-)
+    league_id uuid references leagues(league_id) not null
+);
 
 -- create player statistics table
 create table if not exists player_statistics(
@@ -78,7 +62,7 @@ create table if not exists player_statistics(
  	player_id uuid references players(player_id) unique not null,
  	team_id uuid references teams(team_id) not null,
  	season_id uuid references seasons(season_id) not null
-)
+);
 
 -- create matches table
 create table if not exists matches(
@@ -89,7 +73,7 @@ create table if not exists matches(
     home_team uuid references teams(team_id) not null,
     away_team uuid references teams(team_id) not null,
     league_id uuid references leagues(league_id) not null,
-    season_id uud references seasons(season_id) not null,
+    season_id uuid references seasons(season_id) not null
 );
 
 -- create teams_statistics table: (dont have data for this table, will wait on creating)
