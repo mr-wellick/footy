7afaebc9857a65cc576775066148f29eff5695f8
mr-wellick <lorenz3192@gmail.com>
2024-09-19 02:11:48 -0700

    add jsxRenderer

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,55 @@
 import { Hono } from "hono";
-import { html } from "hono/html";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { swaggerUI } from "@hono/swagger-ui";
 import leagues from "./services/leagues";
 import teams from "./services/teams";
 import Layout from "./views/layout";
 import { swaggerDoc } from "../doc/swaggerDoc";
+import { jsxRenderer } from "hono/jsx-renderer";
 
 const app = new Hono();
 
 // static files
 app.use("/src/public/*", serveStatic({ root: "./" }));
 
+app.use(
+  "/*",
+  jsxRenderer(
+    ({ children }) => {
+      return (
+        <html data-theme="cyberpunk">
+          <head>
+            <meta charset="UTF-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <title>Footy</title>
+            <link href="/src/public/output.css" rel="stylesheet" />
+          </head>
+          <body>{children}</body>
+          <script
+            src="https://unpkg.com/htmx.org@1.9.10"
+            integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC"
+            crossorigin="anonymous"
+          ></script>
+          <script src="https://unpkg.com/htmx-ext-response-targets@2.0.0/response-targets.js"></script>
+        </html>
+      );
+    },
+    {
+      docType:
+        '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
+    },
+  ),
+);
+
 // api documentation
 app.get("/doc", (c) => c.json(swaggerDoc));
 app.get("/ui", swaggerUI({ url: "/doc" }));
 
 // routes
-app.get("/", (c) => c.html(html`<!doctype html>${(<Layout />)}`));
+app.get("/", (c) => c.render(<Layout />));
 app.route("api/v1/leagues", leagues);
 app.route("api/v1/teams", teams);
 
--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -4,24 +4,10 @@ import Leagues from "./components/leagues";
 
 const Layout: FC = () => {
   return (
-    <html data-theme="cyberpunk">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Footy</title>
-        <link href="/src/public/output.css" rel="stylesheet" />
-      </head>
-      <body>
-        <Navbar />
-        <Leagues />
-      </body>
-      <script
-        src="https://unpkg.com/htmx.org@1.9.10"
-        integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC"
-        crossorigin="anonymous"
-      ></script>
-      <script src="https://unpkg.com/htmx-ext-response-targets@2.0.0/response-targets.js"></script>
-    </html>
+    <>
+      <Navbar />
+      <Leagues />
+    </>
   );
 };
 
