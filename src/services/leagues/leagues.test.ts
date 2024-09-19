c789316cba4c95a8cb53b6a55a06f17d01ae33ef
mr-wellick <lorenz3192@gmail.com>
2024-09-18 23:06:36 -0700

    remove sinon and add todo test

--- src/services/leagues/leagues.test.ts
+++ src/services/leagues/leagues.test.ts
@@ -1,34 +1,13 @@
-import { afterEach, beforeEach, describe, it } from "node:test";
+import { describe, it } from "node:test";
 import assert from "node:assert";
-import prisma from "../../db";
-import sinon from "sinon";
 import app from "../../app";
 
-describe("Successful response", () => {
-  it("200 status code: return drop down with all available leagues, if any ", async () => {
+describe("Leagues Controller", () => {
+  it("200: return drop down with all available leagues, if any ", async () => {
     const res = await app.request("api/v1/leagues");
 
     assert.strictEqual(200, res.status);
   });
-});
 
-describe("An unssuccesfull response", () => {
-  // this is hella hacky. need to figure out how to do this properly
-  const originalFindMany = prisma.leagues.findMany;
-  beforeEach(() => {
-    prisma.leagues.findMany = sinon
-      .stub(prisma.leagues, "findMany")
-      .rejects(new Error("Database error"));
-  });
-
-  it("500 status code: return an error view", async () => {
-    const res = await app.request("api/v1/leagues");
-
-    assert.equal(res.status, 500);
-  });
-
-  afterEach(() => {
-    sinon.restore();
-    prisma.leagues.findMany = originalFindMany;
-  });
+  it("500: return an error view", { todo: true });
 });
--- src/services/teams/teams.test.ts
+++ src/services/teams/teams.test.ts
@@ -1,7 +1,5 @@
-import { afterEach, beforeEach, describe, it } from "node:test";
+import { describe, it } from "node:test";
 import assert from "node:assert";
-import prisma from "../../db";
-import sinon from "sinon";
 import app from "../../app";
 
 describe("Teams Controller", async () => {
@@ -24,34 +22,6 @@ describe("Teams Controller", async () => {
 
     assert.strictEqual(404, res.status);
   });
-});
 
-// read through some docs & examples to figure out how to do this right
-describe("Teams Controller", () => {
-  const originalFindMany = prisma.teams.findMany;
-  beforeEach(() => {
-    prisma.teams.findMany = sinon
-      .stub(prisma.teams, "findMany")
-      .rejects(new Error("Database Error"));
-  });
-
-  it("500: should returns an error view when db query fails", async () => {
-    sinon.stub(prisma.teams, "findMany").rejects(new Error("Database Error"));
-
-    const res = await app.request("/api/v1/teams", {
-      method: "POST",
-      body: new URLSearchParams({
-        league_id: "81fd5d4e-d500-413f-881d-6ec5cd2d5222",
-      }).toString(),
-      headers: new Headers({
-        "Content-Type": "application/x-www-form-urlencoded",
-      }),
-    });
-    assert.equal(res.status, 500);
-  });
-
-  afterEach(() => {
-    sinon.restore();
-    prisma.teams.findMany = originalFindMany;
-  });
+  it("500: should return an error view when db query fails", { todo: true });
 });
