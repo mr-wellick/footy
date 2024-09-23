af34da35f494afcf5eb51afe3dbf7c46c884c73e
mr-wellick <lorenz3192@gmail.com>
2024-09-22 19:27:32 -0700

    demo ui
    	- work in progress

--- src/app.tsx
+++ src/app.tsx
@@ -4,40 +4,12 @@ import { swaggerUI } from "@hono/swagger-ui";
 import leagues from "./services/leagues";
 import teams from "./services/teams";
 import { swaggerDoc } from "../doc/swaggerDoc";
-import { jsxRenderer } from "hono/jsx-renderer";
-import Navbar from "./views/components/navbar";
 
 const app = new Hono();
 
 // static files
-app.use("/src/public/*", serveStatic({ root: "./" }));
-
-// app.use(
-//   "/*",
-//   jsxRenderer(
-//     ({ children }) => {
-//       return (
-//         <html>
-//           <head>
-//             <meta charset="UTF-8" />
-//             <meta
-//               name="viewport"
-//               content="width=device-width, initial-scale=1.0"
-//             />
-//             <title>Footy</title>
-//             {/* <link href="/src/public/output.css" rel="stylesheet" /> */}
-//           </head>
-//           <body>{children}</body>
-//         </html>
-//       );
-//     },
-//     {
-//       docType:
-//         '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
-//     }
-//   )
-// );
-app.use("/*", serveStatic({ rewriteRequestPath: (path) => `./dist${path}` }));
+//app.use("/src/public/*", serveStatic({ root: "./" }));
+app.use("/", serveStatic({ rewriteRequestPath: (path) => `./dist${path}` }));
 
 // api documentation
 app.get("/doc", (c) => c.json(swaggerDoc));
